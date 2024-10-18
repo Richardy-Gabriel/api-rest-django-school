4b76951529ac59d1792a788301936a2413fa1095
Richardy Gabriel Rodrigues da Costa <richardyrodrigues519@gmail.com>
2024-10-18 16:00:35 -0300

    Create requirements and configure settings for the rest framework

--- school/settings.py
+++ school/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
+    'django_filters',
+    'rest_framework',
+
     'courses',
 ]
 
@@ -128,3 +131,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# DRF
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+    )
+}
